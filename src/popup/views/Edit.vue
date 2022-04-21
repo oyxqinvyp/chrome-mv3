7822440164c86500dc14093fe67c02bc645d31ad
oyxqin <3490556933@qq.com>
2022-04-21 15:23:03 +0800

    提交

--- src/popup/views/Edit.vue
+++ src/popup/views/Edit.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>
-    编辑任务
+  <div clang="page-edit">
+    <a-form layout="vertical" :model="formState" @finish="onFinish">
+      <a-form-item label="店铺">
+
+      </a-form-item>
+      <a-form-item label="报告类型">
+
+      </a-form-item>
+      <a-form-item label="时间范围">
+
+      </a-form-item>
+      <a-form-item>
+
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue'
 export default defineComponent({
-  name: 'Check'
-});
+  name: 'Check',
+  setup() {
+    const formState = reactive({})
+
+    return {
+      formState,
+    }
+  },
+})
 </script>
+
+<style lang="less">
+.page-edit {
+  width: 350px;
+}
+</style>
